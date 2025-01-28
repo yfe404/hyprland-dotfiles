f26e256157d0547532069b9116e49fc2ef291cb7
yfe404 <yfe.github@protonmail.com>
2025-01-28 16:51:00 +0100

    moved package manager config to init.lua + catppuccin

---
+++ nvim/init.lua
@@ -0,0 +1,37 @@
+local opt = vim.opt
+
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+
+opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+
+require("lazy").setup({plugins, {}})
+require("catppuccin").setup(
+	{
+		flavour = "mocha"
+	}
+)	
+vim.cmd.colorscheme  "catppuccin"
+
+
+
+
