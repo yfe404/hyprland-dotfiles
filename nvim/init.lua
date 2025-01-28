69fa28b1073b438b2397a967309ac7f7b4c92edb
yfe404 <yfe.github@protonmail.com>
2025-01-28 18:35:53 +0100

    add telescope help tags fuzzy searh

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,3 +43,4 @@ vim.cmd.colorscheme  "catppuccin"
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
