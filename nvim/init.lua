e43830d99f586a239614e9206dfbd172a7eb8134
yfe404 <yfe.github@protonmail.com>
2025-01-28 18:08:58 +0100

    add telescope file buffer

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,3 +42,4 @@ vim.cmd.colorscheme  "catppuccin"
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
