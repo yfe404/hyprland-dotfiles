29c7b6e57dc4735f8a66634bbdb481de15c34f5d
yfe404 <yfe.github@protonmail.com>
2025-01-28 18:06:05 +0100

    add telescope live grep
    which is fucking amazing tbh

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,4 +41,4 @@ require("catppuccin").setup(
 vim.cmd.colorscheme  "catppuccin"
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
