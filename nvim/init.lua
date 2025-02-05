2e51ba23c9b5587638a5f6974627a21c02a892a7
yfe404 <yfe.github@protonmail.com>
2025-02-05 08:44:42 +0100

    add config for 42 piscine

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,3 +54,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+
+-- Source the Vimscript file for 42 picine
+vim.cmd('source ~/.config/nvim/vimfiles/42.vim')
